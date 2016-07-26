798287d0e7acc4aca55a677a7b7e361a260da905
Kevin Skompinski <kevons5252@gmail.com>
2016-07-25 23:28:25 -0400

    Initial Commit

---
+++ To-Do/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  To-Do
+//
+//  Created by Kevin on 7/25/16.
+//  Copyright © 2016 Kevin Skompinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
